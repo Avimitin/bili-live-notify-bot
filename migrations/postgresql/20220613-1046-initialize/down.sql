cd28ab08c28dde3a58f97ae7a478fa9652efcdad
Avimitin <avimitin@gmail.com>
2022-06-13 11:04:19 +0800

    db: add initial scripts
    Signed-off-by: Avimitin <avimitin@gmail.com>

---
+++ migrations/postgresql/20220613-1046-initialize/down.sql
@@ -0,0 +1,4 @@
+DROP TABLE register_relation;
+DROP TABLE live_rooms;
+DROP TABLE chats;
+DROP TABLE live_status;
---
+++ migrations/postgresql/20220613-1046-initialize/up.sql
@@ -0,0 +1,24 @@
+CREATE TABLE live_status (
+  status VARCHAR(5) PRIMARY KEY
+);
+
+INSERT INTO live_status(status)
+VALUES ('LIVE'), ('SLEEP'), ('LOOP');
+
+CREATE TABLE chats (
+  id SERIAL PRIMARY KEY,
+  chat_id BIGINT NOT NULL
+);
+
+CREATE TABLE live_rooms (
+  id SERIAL PRIMARY KEY,
+  room_id BIGINT NOT NULL,
+  last_status VARCHAR(5) REFERENCES live_status(status),
+  last_query_time time
+);
+
+CREATE TABLE register_relation (
+  id SERIAL PRIMARY KEY,
+  rid BIGINT NOT NULL REFERENCES live_rooms(id),
+  cid BIGINT NOT NULL REFERENCES chats(id)
+);
