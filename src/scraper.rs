a235a104acd61daa3d3f56517502767f51d971c4
Avimitin <avimitin@gmail.com>
2022-06-13 23:39:40 +0800

    scraper: move test to integration test
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/scraper.rs
+++ src/scraper.rs
@@ -34,10 +34,3 @@ impl MultiLiveRoomStatus {
             .with_context(|| "fail to transcribe response to MultiLiveRoomStatus")?)
     }
 }
-
-#[tokio::test]
-async fn test_multi_live_room_status_scraper() {
-    let multi_room = MultiLiveRoomStatus::new(vec![269415357, 730732]).await.unwrap();
-    assert_eq!(multi_room.data()["730732"].room_id, 42062);
-    assert_eq!(multi_room.data()["269415357"].room_id, 7688602);
-}
