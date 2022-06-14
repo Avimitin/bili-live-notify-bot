5979ed16dbb16bd67019ea24974aed10152328bc
Avimitin <avimitin@gmail.com>
2022-06-14 23:37:53 +0800

    tests: add get_live_room_status test
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- tests/live_room_test.rs
+++ tests/live_room_test.rs
@@ -1,4 +1,7 @@
-use bili_live_notify::db::{PgsqlRepoOperator, RepoOperator};
+use bili_live_notify::{
+    db::{PgsqlRepoOperator, RepoOperator},
+    response_type::LiveStatus,
+};
 use dotenv::dotenv;
 use std::{future::Future, sync::Arc};
 
@@ -20,25 +23,56 @@ lazy_static::lazy_static! {
     static ref PG_DB: PgsqlRepoOperator = PgsqlRepoOperator::from_arc(RAW_PG_CONN.clone());
 }
 
-fn drive<F: Future>(fut: F) -> F::Output {
+fn block_on<F: Future>(fut: F) -> F::Output {
     ASYNC_RT.block_on(fut)
+}
+
+async fn clean_up() {
+    sqlx::query(r#"DELETE FROM register_relation;"#)
+        .execute(&**RAW_PG_CONN)
+        .await
+        .expect("fail to clean up register_relation");
+    sqlx::query(r#"DELETE FROM live_rooms;"#)
+        .execute(&**RAW_PG_CONN)
+        .await
+        .expect("fail to clean up live_rooms");
+    sqlx::query(r#"DELETE FROM chats;"#)
+        .execute(&**RAW_PG_CONN)
+        .await
+        .expect("fail to clean up chats");
 }
 
 #[test]
 fn test_add_live_room() {
     let expect = 1919810;
-    let id = drive(PG_DB.add_live_room(expect)).expect("Fail to add live room");
+    let id = block_on(PG_DB.add_live_room(expect)).expect("Fail to add live room");
 
-    let fut = sqlx::query!(r#"SELECT room_id FROM live_rooms WHERE id = $1"#, id)
+    let fut = sqlx::query!(r#"SELECT room_id FROM live_rooms WHERE id = $1;"#, id)
         .fetch_one(&**RAW_PG_CONN);
-    let ret = drive(fut);
-
-    if ret.is_err() {
-        eprintln!("fail to get live room to compare: {:?}", ret);
-        return;
-    }
-
-    let resp = ret.unwrap();
-    let get = resp.room_id;
+    let ret = block_on(fut).expect("fail to get live room to compare");
+    let get = ret.room_id;
     assert_eq!(get, expect);
+
+    block_on(clean_up());
+}
+
+#[test]
+fn test_get_live_room_status() {
+    let room_id = 1919810;
+    let expect = LiveStatus::Sleep;
+    let qry = sqlx::query!(
+        r#"
+INSERT INTO live_rooms (room_id, last_status)
+VALUES ($1, $2);"#,
+        room_id,
+        expect.as_str()
+    )
+    .execute(&**RAW_PG_CONN);
+    block_on(qry).expect("fail to insert status into live room");
+
+    let get = block_on(PG_DB.get_live_room_status(room_id)).expect("fail to get live room status");
+
+    assert_eq!(get, expect);
+
+    block_on(clean_up());
 }
