b8d0767b3daf4cb093b567c70bd2bc0066e36226
Avimitin <avimitin@gmail.com>
2022-06-19 18:31:59 +0800

    db: add get_pending_room function
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,12 @@
 use crate::models::*;
 use crate::response_type::LiveStatus;
 use crate::schema::rooms;
+use diesel::dsl::IntervalDsl;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use std::fmt::Display;
 use std::sync::Arc;
+use tokio::sync::Mutex;
 
 #[derive(Debug)]
 pub enum GetRoomQueryParams {
@@ -29,32 +31,37 @@ pub enum RepoOperationError<T: Display> {
     InvalidQueryParamError,
     #[error("No result found: {msg}")]
     NoExpectResultFoundError { msg: String },
+    #[error("Fail to get pending rooms: {msg}")]
+    GetPendingRoomsError { msg: String, source: T },
 }
 
 /// Repo keeps a immutable reference to the PostgreSQL connection pool.
 /// It capsulate most of the database operations
+#[derive(Clone)]
 pub struct Repo {
-    conn: Arc<PgConnection>,
+    conn: Arc<Mutex<PgConnection>>,
 }
 
 impl Repo {
     /// Create a new struct to hold the given connection to PostgreSQL.
     pub fn new(conn: PgConnection) -> Self {
         Self {
-            conn: Arc::new(conn),
+            conn: Arc::new(Mutex::new(conn)),
         }
     }
 
+    /// Get room status by room id or local id.
     pub async fn get_room_status(
         &self,
         id: Option<i32>,
         room_id: Option<i64>,
     ) -> Result<LiveStatus, RepoOperationError<diesel::result::Error>> {
+        let conn = self.conn.lock().await;
         let room = tokio::task::block_in_place(move || {
             if let Some(id) = id {
                 rooms::table
                     .filter(rooms::id.eq_all(id))
-                    .first::<Rooms>(&*self.conn)
+                    .first::<Rooms>(&*conn)
                     .map_err(|e| RepoOperationError::GetRoomStatusError {
                         param: GetRoomQueryParams::Id(id),
                         raw: e,
@@ -62,23 +69,47 @@ impl Repo {
             } else if let Some(rid) = room_id {
                 rooms::table
                     .filter(rooms::room_id.eq_all(rid))
-                    .first::<Rooms>(&*self.conn)
+                    .first::<Rooms>(&*conn)
                     .map_err(|e| RepoOperationError::GetRoomStatusError {
                         param: GetRoomQueryParams::RoomId(rid),
                         raw: e,
                     })
             } else {
-                return Err(RepoOperationError::InvalidQueryParamError);
+                Err(RepoOperationError::InvalidQueryParamError)
             }
         })?;
 
-        let status =
-            room.last_status
-                .ok_or_else(|| RepoOperationError::NoExpectResultFoundError {
-                    msg: format!("No status found for room {}", room.room_id),
-                })?;
-
-        Ok(LiveStatus::from(status)
+        Ok(LiveStatus::from(room.status)
             .expect("Unexpect live status appear, please check database health"))
+    }
+
+    /// Get all rooms that has outdated status and is pending for querying.
+    pub async fn get_pending_rooms(
+        &self,
+    ) -> Result<Vec<i64>, RepoOperationError<diesel::result::Error>> {
+        let conn = self.conn.lock().await;
+
+        // TODO: make interval value configurable
+        let rooms = tokio::task::block_in_place(move || {
+            rooms::table
+                .filter(rooms::updated_at.gt(diesel::dsl::now + 1.minutes()))
+                .select(rooms::room_id)
+                .load::<i64>(&*conn)
+                .map_err(|e| RepoOperationError::GetPendingRoomsError {
+                    msg: "fail to load all pending rooms".to_string(),
+                    source: e,
+                })
+        })?;
+
+        Ok(rooms)
+    }
+
+    /// This function should only be used when we are testing/debugging
+    #[cfg(debug_assertions)]
+    pub async fn raw_execute(&self, query: &str) {
+        let conn = self.conn.lock().await;
+        diesel::sql_query(query)
+            .execute(&*conn)
+            .unwrap_or_else(|_| panic!("fail to execute query: {query}"));
     }
 }
--- tests/live_room_test.rs
+++ tests/live_room_test.rs
@@ -1,19 +1,80 @@
+use std::future::Future;
+
+/// TEST SHOULD BE RUN INDIVIDUALLY
+/// USE `test-db.sh` script! DON'T RUN `cargo test`
 use bili_live_notify::db::Repo;
 use bili_live_notify::response_type::LiveStatus;
 
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenv::dotenv;
+use tokio::runtime::Runtime;
 
-#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
-async fn test_get_live_rooms() {
-    let conn: PgConnection = {
-        dotenv().ok();
-        let addr = std::env::var("DATABASE_URL")
-            .expect("no integration database address found");
-        PgConnection::establish(&addr).expect("fail to connect to pg database")
+lazy_static::lazy_static! {
+    static ref RT: Runtime = {
+        tokio::runtime::Builder::new_multi_thread()
+            .worker_threads(1)
+            .build()
+            .expect("fail to build tokio runtime builder")
     };
-    let repo: Repo = Repo::new(conn);
+    static ref DB: Repo = {
+        let conn: PgConnection = {
+            dotenv().ok();
+            let addr = std::env::var("DATABASE_URL").expect("no integration database address found");
+            PgConnection::establish(&addr).expect("fail to connect to pg database")
+        };
+        Repo::new(conn)
+    };
+}
 
-    assert_eq!(repo.get_room_status(Some(1), None).await.expect("Fail to get status"), LiveStatus::Living);
+fn block<F: Future>(f: F) -> F::Output {
+    RT.block_on(f)
+}
+
+fn block_execute(s: &str) {
+    block(async {
+        DB.raw_execute(s).await;
+    })
+}
+
+fn clean_up() {
+    block(async {
+        DB.raw_execute("DELETE FROM chats;").await;
+        DB.raw_execute("DELETE FROM rooms;").await;
+        DB.raw_execute("DELETE FROM regis;").await;
+    });
+}
+
+#[test]
+fn test_get_live_rooms() {
+    clean_up();
+
+    block_execute(
+        r#"
+INSERT INTO rooms (room_id, status, updated_at)
+VALUES (12345, 1, NOW());
+"#,
+    );
+
+    assert_eq!(
+        block(DB.get_room_status(None, Some(12345))).expect("Fail to get status"),
+        LiveStatus::Living
+    );
+}
+
+#[test]
+fn test_get_pending_rooms() {
+    clean_up();
+
+    block_execute(
+        r#"
+INSERT INTO rooms (room_id, status, updated_at)
+VALUES (12345, 1, NOW() + interval '2' minute), (67890, 0, NOW() + interval '2' minute);
+"#,
+    );
+
+    assert_eq!(
+        block(DB.get_pending_rooms()).expect("Fail to get pending rooms"),
+        vec![12345, 67890]
+    )
 }
