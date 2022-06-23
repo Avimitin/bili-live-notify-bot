589fac3d15b871852b49d98df99e5c8efc91aac8
Avimitin <avimitin@gmail.com>
2022-06-23 23:33:04 +0800

    db: add last_query_at into rooms
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- migrations/2022-06-15-100859_initialize/up.sql
+++ migrations/2022-06-15-100859_initialize/up.sql
@@ -4,12 +4,13 @@ CREATE TABLE chats (
 );
 
 CREATE TABLE rooms (
-  id         SERIAL PRIMARY KEY,     -- unique id in local
-  room_id    BIGINT NOT NULL UNIQUE, -- room id for fetching
-  uname      TEXT,                   -- room's owner user name
-  status     INT NOT NULL,           -- room's last status
-  updated_at TIMESTAMP NOT NULL,     -- last query time for this rooms
-  archive    BOOLEAN                 -- true if no chats register this room
+  id            SERIAL PRIMARY KEY,             -- unique id in local
+  room_id       BIGINT NOT NULL UNIQUE,         -- room id for fetching
+  uname         TEXT,                           -- room's owner user name
+  status        INT NOT NULL,                   -- room's last status
+  last_query_at TIMESTAMP,                      -- last query time for this rooms
+  updated_at    TIMESTAMP NOT NULL              -- last update time for this row
+  archive       BOOLEAN NOT NULL DEFAULT FALSE  -- true if no chats register this room
 );
 
 CREATE TABLE regis (
